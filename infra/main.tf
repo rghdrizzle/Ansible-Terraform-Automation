9926e766a9f648fd322bb789df4c076d545ba3c6
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2023-06-09 18:38:55 +0530

    Updated main.tf
    added network security group association to the subnet

--- infra/main.tf
+++ infra/main.tf
@@ -18,6 +18,24 @@ resource "azurerm_public_ip" "ip" {
   allocation_method   = "Static"
 }
 
+resource "azurerm_network_security_group" "netsg" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = var.rglocation
+  resource_group_name = var.rgname
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_virtual_network" "main" {
   name                = "ansible-network"
   address_space       = ["10.0.0.0/16"]
@@ -25,25 +43,33 @@ resource "azurerm_virtual_network" "main" {
   resource_group_name = var.rgname
 }
 
-resource "azurerm_subnet" "internal" {
+resource "azurerm_subnet" "SubnetA" {
   name                 = "internal"
   resource_group_name  = var.rgname
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "neti" {
-  name                = "ansible-nic"
+resource "azurerm_network_interface" "app_interface" {
+  name                = "ansible"
   location            = var.rglocation
   resource_group_name = var.rgname
 
   ip_configuration {
-    name                          = "testconfig-ip"
-    subnet_id                     = azurerm_subnet.internal.id
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.SubnetA.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.ip.id
+    public_ip_address_id = azurerm_public_ip.ip.id
   }
+
+  depends_on = [
+    azurerm_virtual_network.main,
+    azurerm_public_ip.ip,
+    azurerm_subnet.SubnetA
+  ]
 }
+
+
 
 resource "azurerm_linux_virtual_machine_scale_set" "ansible" {
   name                = "ansible-vmss"
@@ -65,20 +91,25 @@ resource "azurerm_linux_virtual_machine_scale_set" "ansible" {
     version   = "latest"
   }
 
-  network_interface {
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+   network_interface {
     name    = "ansible"
     primary = true
 
     ip_configuration {
       name      = "internal"
-      primary   = true
-      subnet_id = azurerm_subnet.internal.id
-      version = "IPv6"
+      subnet_id = azurerm_subnet.SubnetA.id
     }
   }
 
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
-  }
+}
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.SubnetA.id
+  network_security_group_id = azurerm_network_security_group.netsg.id
+  depends_on = [
+    azurerm_network_security_group.netsg
+  ]
 }
